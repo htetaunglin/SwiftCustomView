3296b3f47a39cb126f7543292ebed643a6acf8e5
Thet Tun <thethtun4040@gmail.com>
2021-12-19 07:09:24 +0630

    Initial Commit

---
+++ HouseHunter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HouseHunter
+//
+//  Created by Thet Htun on 12/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
